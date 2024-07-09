8ecd36a2f06a73823acc2da4330255096bc43a16
Nishi-1805 <165827696+Nishi-1805@users.noreply.github.com>
2024-07-09 17:14:50 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -2,21 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const sequelize = require('./util/database');
-const playerController = require('./controllers/PlayerController');
-const Player = require('./models/playerModel');
 const routes = require('./routes/playerRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/player.html');
 });
-app.use(routes);
+app.use('/players', routes);
 
-sequelize.sync()
-  .then(() => {
+sequelize.sync() // Use { force: true } in development mode
+.then(() => {
     console.log('Database schema synchronized');
     app.listen(3000, () => {
       console.log('Server listening on port 3000');
@@ -24,6 +22,6 @@ sequelize.sync()
       console.error('Error starting server:', error);
     });
   })
-  .catch((error) => {
+.catch((error) => {
     console.error('Error synchronizing database schema:', error);
   });

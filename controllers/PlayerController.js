e06fca868884bcdf3e3ff38f16fd218e25968214
Nishi-1805 <165827696+Nishi-1805@users.noreply.github.com>
2024-07-17 17:11:00 +0530

    Update PlayerController.js

--- controllers/PlayerController.js
+++ controllers/PlayerController.js
@@ -1,5 +1,6 @@
 const Player = require('../models/playerModel');
 const path = require('path');
+const { Op } = require('sequelize');
 
 exports.createPlayer = async (req, res) => {
   try {
@@ -46,5 +47,81 @@ exports.getPlayerData = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Failed to retrieve player' });
+  }
+};
+
+// In your server-side code
+exports.getEditPlayer = async (req, res) => {
+  const playerId = req.params.id;
+  try {
+    const player = await Player.findByPk(playerId);
+    if (!player) {
+      res.status(404).json({ error: 'Player not found' });
+    } else {
+      res.sendFile(path.join(__dirname, '../views/player.html'), {
+        playerId: playerId,
+        playerData: JSON.stringify(player.dataValues)
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to retrieve player' });
+  }
+};
+
+exports.updatePlayer = async (req, res) => {
+  const playerId = req.body.playerId;
+  const updatedData = req.body;
+
+  try {
+    const player = await Player.findByPk(playerId);
+    if (!player) {
+      res.status(404).json({ error: 'Player not found' });
+    } else {
+      player.name = updatedData.name;
+      player.dateOfBirth = new Date(updatedData.dateOfBirth); // parse date string to timestamp
+      player.photoUrl = updatedData.photoUrl; // update photo URL
+      player.birthPlace = updatedData.birthPlace;
+      player.career = updatedData.career; // update career information
+      player.numberOfMatches = updatedData.numberOfMatches;
+      player.score = updatedData.score;
+      player.fifties = updatedData.fifties;
+      player.centuries = updatedData.centuries;
+      player.wickets = updatedData.wickets;
+      player.averages = updatedData.averages;
+
+      await player.save();
+      console.log('Player updated:', player);
+      res.json(player);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to update player' });
+  }
+};
+
+exports.searchPlayer = async (req, res) => {
+  console.log('Search player function called'); 
+  const searchQuery = req.query.name;
+  try {
+    const player = await Player.findOne({
+      where: {
+        name: {
+          [Op.like]: `%${searchQuery}%`
+        }
+      }
+    });
+    console.log(`Player found: ${player}`);
+    if (player) {
+      console.log(`Redirecting to /players/${player.id}`);
+      res.redirect(`/players/${player.id}`); // Redirect to player details page
+    } else {
+      res.status(404);
+      res.json({ error: 'Player not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.json({ error: 'Failed to search for player' });
   }
 };

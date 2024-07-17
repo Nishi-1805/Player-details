a38c7c87cbfa98845f8da97f488a9c91ab9309a0
Nishi-1805 <165827696+Nishi-1805@users.noreply.github.com>
2024-07-17 20:27:03 +0530

    Update playerRoutes.js

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const playerController = require('../controllers/PlayerController');
 
+router.get('/search', playerController.searchPlayer);
 router.post('/', playerController.createPlayer);
 router.get('/:id', playerController.getPlayer);
 router.get('/:id/data', playerController.getPlayerData);
 router.get('/:id/edit', playerController.getEditPlayer);
 router.put('/:id', playerController.updatePlayer);
 
-console.log('Defining search route...');
-router.get('/players/search', playerController.searchPlayer);
 
 module.exports = router;
